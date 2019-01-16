44dd5801ecc81eabd8a183e85e2cc07c053b0ccb
Festus Iipito <dev3320770@gmail.com>
2019-01-15 20:27:52 -0600

    homework01 (exercise1)

--- homework_01/exercise2.js
+++ homework_01/exercise2.js
@@ -2,8 +2,6 @@ function isWeekend() {
 	const todayDate = new Date();
 		const day = todayDate.getDay();
 
-
-
   	const days = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend']
     return days[day];
 }
