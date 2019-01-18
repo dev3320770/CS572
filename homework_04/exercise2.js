ccf4c95ec126ca99b10e2a38ffb0978443ac7fb6
Festus Iipito <dev3320770@gmail.com>
2019-01-18 13:33:38 -0600

    homework04

--- homework_04/exercise2.js
+++ homework_04/exercise2.js
@@ -1,25 +1,29 @@
 const http = require("http");
 const {fork} = require("child_process");
 const server = http.createServer();
+const {Observable}  = require("rxjs");
 
-
-server.on("request",(req, res)=>{
-if(req.url === "/"){
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Home Page</h1><br /><br /><a href="/?url=path/to/my/file.txt">Click ob the link HERE to view data in file.txt</a>');
-    res.end();
-
-}else if(req.url === "/?url=path/to/my/file.txt"){
-  
-    const childProcess = fork("exercise2_heavyOperation.js");
-    childProcess.send("msg");
-    childProcess.on("message", data =>{
-        res.end(data);
-    })
-}else{
-    res.writeHead(404);
-    res.end("Page Not Found");
-}
-
+Observable.create(function (){
+    server.on("request",(req, res)=>{
+        if(req.url === "/"){
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('<h1>Home Page</h1><br /><br /><a href="/?url=path/to/my/file.txt">Click ob the link HERE to view data in file.txt</a>');
+            res.end();
+        
+        }else if(req.url === "/?url=path/to/my/file.txt"){
+          
+            const childProcess = fork("exercise2_heavyOperation.js");
+            childProcess.send("start");
+            childProcess.on("message", data =>{
+                res.end(data);
+            })
+        }else{
+            res.writeHead(404);
+            res.end("Page Not Found");
+        }
+        
+           
+        }).listen(3000, ()=> console.log("Listening on Port 3000"));
+}).subscribe()
    
-}).listen(3000, ()=> console.log("Listening on Port 3000"));
+
