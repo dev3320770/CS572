6489af8e6cbacf65f6faad4ff709789fd5e4f846
Festus Iipito <dev3320770@gmail.com>
2019-01-20 08:35:25 -0600

    homework06

---
+++ homework_06/model/data.js
@@ -0,0 +1,33 @@
+const grades = [{
+        id: 1,
+        name: "Festus Iipito",
+        course: "CS572",
+        grade: 98
+    },
+    {
+        id: 2,
+        name: "Momento",
+        course: "CS572",
+        grade: 97
+    },
+    {
+        id: 3,
+        name: "Tuta Franco",
+        course: "CS472",
+        grade: 37
+    },
+    {
+        id: 4,
+        name: "Maria Namupala",
+        course: "CS382",
+        grade: 67
+    },
+    {
+        id: 5,
+        name: "Hedvig Mendaca",
+        course: "CS572",
+        grade: 97
+    }
+]
+
+module.exports = grades;
