f70b1a5d8e942841ed75cdaac78c950cb7eef368
Festus Iipito <dev3320770@gmail.com>
2019-01-21 21:26:53 -0600

    homework07

--- homework_07/exercise1/schemaDesign.js
+++ homework_07/exercise1/schemaDesign.js
@@ -1,31 +1,32 @@
-{
-    _id: ObjectId(),
-    ISBN: "", //unique
-    author: [{
-        name:{
-            fname:"",
-            lname:""
+var schema = {
+    
+    "ISBN" : "ISBN1323433434", 
+    "author": [{
+        "name":{
+            "fname":"Festus",
+            "lname":"Iipito"
         }
 
     }],
-    tags:["", "", ""],
-    student:[{
-        name:{
-            fname:"",
-            lname:""
+    "tags":["mongoDB", "javascript", "nodeJs"],
+    "student":[{
+        "name":{
+            "fname":"Tuta",
+            "lname":"Franco"
         },
-        studentId:"",
-        email:"",
-        address:[{
-            city:"",
-            town:"",
-            zipCode:"",
-            streetName:""
+        "studentId":986683,
+        "email":"fiipito@mum.edu",
+        "address":[{
+            "city":"Iowa",
+            "town":"Fairfield",
+            "zipCode":"57776",
+            "streetName":"1000 North Street"
         }],
-        checkOutDate: Date,
-        returnDate: Date
+        "checkOutDate": Date.now(),
+        "returnDate": new Date("February 19, 2019 00:00:00"),
+        "status": 1
     }],
-    isAvailable: true
+    "isAvailable": false
 
 
 }
