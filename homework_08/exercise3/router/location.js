f98a3a0c13cf6376fad78bc400e9feb91031c670
Festus Iipito <dev3320770@gmail.com>
2019-01-23 01:38:32 -0600

    homework08

--- homework_08/exercise3/router/location.js
+++ homework_08/exercise3/router/location.js
@@ -6,17 +6,24 @@ const router = express.Router();
 router.get("/",async (req, res)=>{
     const collection = req.collec;
     if(!req.body || !req.body.category){
-        return res.status(400).send("Category required, name is optional")
+        return res.status(400).send("Category required name is optional")
     }
     const searchData = req.body;
   
     const searchFromData = await collection.find(searchData).toArray();
 
-   const log = searchFromData[0].location[0];
+   const long = searchFromData[0].location[0];
    const lat = searchFromData[0].location[1];
 
-    const query = {location:{$near:{$geometry:{coordinates:[log,lat]}}}};
-    const results = await collection.find(query).skip(1).limit(3).toArray();
+    const query = [
+        {
+          $geoNear: {
+             near: { coordinates:[long,lat] },
+             distanceField: "distanceFrom.calculated"
+          }
+        }
+     ];
+    const results = await collection.aggregate(query).skip(1).limit(3).toArray();
     res.send(results);
 
 })
